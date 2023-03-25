4cb039900a9c149c41c470ce8e9cd372dd615491
Huynh Phat <phathuynhz456@gmail.com>
2023-03-25 13:42:46 +0700

    Finished Show Attending and Fix Following

--- HuynhDucPhat_1811062779_BigSchool/Controllers/HomeController.cs
+++ HuynhDucPhat_1811062779_BigSchool/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using HuynhDucPhat_1811062779_BigSchool.ViewModels;
 
 namespace HuynhDucPhat_1811062779_BigSchool.Controllers
 {
@@ -19,12 +20,18 @@ namespace HuynhDucPhat_1811062779_BigSchool.Controllers
 
         public ActionResult Index()
         {
-            var upcommingCourse = _dbContext.Courses
+            var upcommingCourses = _dbContext.Courses
             .Include(c => c.Lecturer)
             .Include(c => c.Category)
             .Where(c => c.DateTime > DateTime.Now);
 
-            return View(upcommingCourse);
+            var viewModel = new CoursesViewModel
+            {
+                UpcommingCourses = upcommingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ HuynhDucPhat_1811062779_BigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HuynhDucPhat_1811062779_BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HuynhDucPhat_1811062779_BigSchool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
