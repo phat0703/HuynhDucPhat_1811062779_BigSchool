6230b773db40f82ec1568bd9b8995ac54a5f7076
Huynh Phat <phathuynhz456@gmail.com>
2023-03-25 11:48:33 +0700

    Finished Going and Fix Following

---
+++ HuynhDucPhat_1811062779_BigSchool/DTOs/AttendanceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HuynhDucPhat_1811062779_BigSchool.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
---
+++ HuynhDucPhat_1811062779_BigSchool/DTOs/FollowingDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HuynhDucPhat_1811062779_BigSchool.DTOs
+{
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+    }
+}
